ea9252b03f2162531039f9371f01859fbfbdd3cc
vamshi-muktha <mukthavamshi123@gmail.com>
2026-01-14 00:51:53 +0530

    Readied for deployment

--- src/main/java/com/vamshi/securecard/securecard/config/Config.java
+++ src/main/java/com/vamshi/securecard/securecard/config/Config.java
@@ -275,7 +275,7 @@ public class Config {
 	public CorsConfigurationSource corsConfigurationSource() {
 
 		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowedOrigins(List.of("https://spring-project-frontend.vercel.app/", "http://localhost:3000", "http://localhost:5173"));
+		config.setAllowedOrigins(List.of("https://spring-project-frontend.vercel.app", "http://localhost:3000", "http://localhost:5173"));
 		config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		config.setAllowedHeaders(List.of("*"));
 		config.setAllowCredentials(true);
